c5ada29cebffdb893b32fddca5e501cf3052895a
apparao <apparao1289@gmail.com>
2020-08-28 17:34:45 +0530

    Added DB scripts.

---
+++ springboot-jpa-example/Readme.txt.bak
@@ -0,0 +1,17 @@
+Download the code base .
+
+Eclipse --> File --> import --> Existing maven projects --> select the project code base.
+
+Eclipse --> Run --> Run Configuration --> left side window select "Maven Build" --> Name ( student service clean package)
+
+Goals: clean package
+
+Click on “Apply” button
+
+Click on “Run” button.
+
+For running the application open SpringbootJpaExampleApplication.java class and right click ? Run As Spring Boot App
+
+Open your favorite browser, paste the below swagger link and hit enter
+
+http://localhost:8081/studentapi/swagger-ui.html
---
+++ springboot-scheduling/src/main/resources/STUDENT.sql
@@ -0,0 +1,16 @@
+REM INSERTING into STUDENT
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (1,'SURESH',21,'MIAMI FL','15');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (2,'RAMESH',23,'ATLANATA GA','19');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (3,'SURESH',21,'MIAMI FL','15');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (4,'TOM',19,'NEW JERSEY','14');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (5,'BOB',24,'NEW YORK','19');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (6,'apparao G',30,'Bangalore','19');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (7,'Anand B',25,'US','19');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (8,'suresh rao B',30,'Chennai','18');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (9,'satya G',12,'Chennai','15');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (10,'krishna P',30,'US','19');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (11,'Ramarao P',30,'US','20');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (12,'Amish A',12,'Chennai','15');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (13,'Rao G',16,'Bangalore','18');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (14,'Rama rao G',16,'Chennai','18');
+Insert into STUDENT (ID,NAME,AGE,ADDRESS,STAND) values (16,'Surya',16,'Atlanata Ga','20');
